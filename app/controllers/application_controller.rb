f456d3c5f09893f9f16950342f5a9b401f8327c5
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-09 17:10:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,5 @@
 class ApplicationController < Sinatra::Base
 
-  class Dog
-    attr_accessor :name, :breed, :age
-
-    @@all = []
-
-    def initialize(name, breed, age)
-      @name = name
-      @breed = breed
-      @age = age
-      @@all << self
-    end
-
-    def self.all
-      @@all
-    end
-
-  end
-
-  binding.pry
-
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
