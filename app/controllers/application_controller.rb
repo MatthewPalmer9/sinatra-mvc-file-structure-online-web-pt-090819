2c816ddb89d8864475cb08f499a0e007174a43f2
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-09 17:01:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,15 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+
+  class Dog
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(name, breed, id = nil)
+      @name = name
+      @breed = breed 
+    end
+
+  end
 end
